49c7a1de0864f5af01525409181aa961db684c54
Gary <glswim@gmail.com>
2024-04-17 09:05:21 +0800

    Added hit, miss, placeShip functions

--- src/compounds/Plot.js
+++ src/compounds/Plot.js
@@ -1,5 +1,24 @@
-const plotShip = () =>{
+const plotShip = (name, row, col, length, orientation) =>{
+    console.log({
+        name: name,
+        row: row,
+        col: col,
+        orientation: orientation
+    })
 
+    if(orientation === "horizontal")
+    {
+        for(let index = 0; index < length; index++){
+            const createId = document.getElementById(`${name.toLowerCase()}-${row}-${col + index}`);
+            createId.addEventListener(("click"), e =>{console.log(e.currentTarget)});
+            createId.classList.add("ship");
+        }
+    } else {
+        for(let index = 0; index < length; index++){
+            const createId = document.getElementById(`${name.toLowerCase()}-${row +index}`);
+            createId.classList.add("ship");
+        }
+    }
 }
 
 const plotHit = () =>{
@@ -10,4 +29,12 @@ const plotMiss = () =>{
 
 }
 
-export {plotShip, hit, miss}
+const removePlot = () =>{
+
+}
+
+const clearBoard = () =>{
+
+}
+
+export {plotShip, plotHit, plotMiss, removePlot}
